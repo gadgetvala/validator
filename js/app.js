a3b038bf62a2af7a0c56c4a5d1757552e25547a3
gadgetvala <surajj.verma7@gmail.com>
2020-04-22 07:49:57 +0530

    Update validator

--- js/app.js
+++ js/app.js
@@ -1,113 +1,200 @@
 const inputBar = document.querySelector('.inputbar');
 const inputbarOnly = document.querySelector('#inputbar-only');
 //Alphabet group
-const onlyAlphabet = document.querySelector('#onlyAlphabet');
-const capitalize = document.querySelector('#capitalize');
-const allCaps = document.querySelector('#allCaps');
-const spaceBetween = document.querySelector('#spaceBetween');
+const isAlpha = document.querySelector('#isAlpha');
+const isUppercase = document.querySelector('#isUppercase');
+const islowercase = document.querySelector('#islowercase');
+const isAlphanumeric = document.querySelector('#isAlphanumeric');
 //Number group
-const isNumberic = document.querySelector('#isNumberic');
-const startWith0 = document.querySelector('#startWith0');
-const lessThan100 = document.querySelector('#lessThan100');
-const lessThan10 = document.querySelector('#lessThan10');
+const isNumber = document.querySelector('#isNumber');
+const isHexColor = document.querySelector('#isHexColor');
+const isHexadecimal = document.querySelector('#isHexadecimal');
+const isEmail = document.querySelector('#isEmail');
 //Symbol group
-const onlySpecialChar = document.querySelector('#onlySpecialChar');
-const startWithSpecial = document.querySelector('#startWithSpecial');
-const endWith = document.querySelector('#endWith');
-const emailValidation = document.querySelector('#emailvalidation');
+const isIpAdd = document.querySelector('#isIpAdd');
+const isMACAddress = document.querySelector('#isMACAddress');
+const isMD5 = document.querySelector('#isMD5');
+const isSHA1 = document.querySelector('#isSHA1');
 
 inputbarOnly.addEventListener('change', () => {
 	//Alphabet group
-	if (onlyAlphabet.checked) {
-		if (/^[a-zA-Z]+$/.test(inputbarOnly.value)) {
+	if (isAlpha.checked) {
+		if (isValidAlpha(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
 		} else {
 			inputBar.className += ' wrong';
 		}
 	}
-	if (capitalize.checked) {
-		if (inputbarOnly.value[0] !== inputbarOnly.value[0].toUpperCase()) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isUppercase.checked) {
+		if (isValidUppercase(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (lessThan100.checked) {
-		if (inputbarOnly.value !== inputbarOnly.value.toUpperCase()) {
-			inputBar.className += ' wrong';
-		} else {
+	if (islowercase.checked) {
+		if (isValidlowercase(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (spaceBetween.checked) {
-		if (!/ /.test(inputbarOnly.value)) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isAlphanumeric.checked) {
+		if (isvalidAlphanumeric(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
 
 	//Number Group
-	if (isNumberic.checked) {
-		if (/^[0-9]+$/.test(inputbarOnly.value)) {
+	if (isNumber.checked) {
+		if (isValidNumber(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
 		} else {
 			inputBar.className += ' wrong';
 		}
 	}
-	if (startWith0.checked) {
-		if (!/0.*$/.test(inputbarOnly.value[0])) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isHexColor.checked) {
+		if (isValidHexColor(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (lessThan100.checked) {
-		let total = inputbarOnly.value;
-		if (total >= 100) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isHexadecimal.checked) {
+		if (isValidHexadecimal(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (lessThan10.checked) {
-		let total = inputbarOnly.value;
-		if (total >= 10) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isEmail.checked) {
+		if (isValidEmail(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
 
 	//Symbol group
-	if (onlySpecialChar.checked) {
-		if (/^[^a-zA-Z0-9]+$/.test(inputbarOnly.value)) {
+	if (isIpAdd.checked) {
+		if (isValidIpAdd(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
 		} else {
 			inputBar.className += ' wrong';
 		}
 	}
-	if (startWithSpecial.checked) {
-		if (!/^[^a-zA-Z0-9]+$/.test(inputbarOnly.value[0])) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isMACAddress.checked) {
+		if (isValidMACAddress(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (endWith.checked) {
-		let len = inputbarOnly.value.length;
-		console.log(inputbarOnly.value[len - 1]);
-		if (!/^[^a-zA-Z0-9]+$/.test(inputbarOnly.value[len - 1])) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isMD5.checked) {
+		if (isValidMD5(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
-	if (emailValidation.checked) {
-		if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(inputbarOnly.value)) {
-			inputBar.className += ' wrong';
-		} else {
+	if (isSHA1.checked) {
+		if (isValidSHA1(inputbarOnly.value)) {
 			inputBar.className = 'inputbar';
+		} else {
+			inputBar.className += ' wrong';
 		}
 	}
 });
+
+function isValidIpAdd(ipaddress) {
+	if (
+		/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+			ipaddress
+		)
+	) {
+		return true;
+	}
+	return false;
+}
+
+function isValidMACAddress(macaddress) {
+	if (/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.test(macaddress)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidMD5(MD5) {
+	if (/^[a-fA-F0-9]{32}$/.test(MD5)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidSHA1(SHA1) {
+	if (/^[a-fA-F0-9]{40}$/.test(SHA1)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidEmail(email) {
+	if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidAlpha(alpha) {
+	if (/^[a-zA-Z]+$/.test(alpha)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidUppercase(alpha) {
+	if (/^[A-Z]+$/.test(alpha)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidlowercase(alpha) {
+	if (/^[a-z]+$/.test(alpha)) {
+		return true;
+	}
+	return false;
+}
+
+function isvalidAlphanumeric(alpha) {
+	if (/^[a-zA-Z0-9_]*$/.test(alpha)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidNumber(num) {
+	if (/^[0-9]+$/.test(num)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidHexColor(code) {
+	if (/^#(?:[0-9a-f]{3}){1,2}$/i.test(code)) {
+		return true;
+	}
+	return false;
+}
+
+function isValidHexadecimal(code) {
+	if (/0[xX][0-9a-fA-F]+/.test(code)) {
+		return true;
+	}
+	return false;
+}
